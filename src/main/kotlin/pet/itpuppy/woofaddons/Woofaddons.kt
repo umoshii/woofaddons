e3bee109baf9c1b9d6fe235802b8b2b5986ae501
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2026-09-08 20:01:23 +0200

    feat: held type thing

--- src/main/kotlin/pet/itpuppy/woofaddons/Woofaddons.kt
+++ src/main/kotlin/pet/itpuppy/woofaddons/Woofaddons.kt
@@ -4,6 +4,8 @@ import net.fabricmc.api.ModInitializer
 import net.minecraft.resources.Identifier
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import pet.itpuppy.woofaddons.commands.ServerCommand
+import pet.itpuppy.woofaddons.events.ChatEvent
 
 object Woofaddons : ModInitializer {
 	const val MOD_ID: String = "woofaddons"
@@ -11,6 +13,13 @@ object Woofaddons : ModInitializer {
 
 	override fun onInitialize() {
 		LOGGER.info("woof!")
+
+        //return // Return when testing in singleplayer
+        ChatEvent.register()
+
+        ServerCommand::class.sealedSubclasses
+            .mapNotNull { it.objectInstance }
+            .forEach { it.register() }
 	}
 
 	fun id(path: String): Identifier = Identifier.fromNamespaceAndPath(MOD_ID, path)
--- src/main/kotlin/pet/itpuppy/woofaddons/commands/ShowCommand.kt
+++ src/main/kotlin/pet/itpuppy/woofaddons/commands/ShowCommand.kt
@@ -1,11 +1,16 @@
 package pet.itpuppy.woofaddons.commands
 
+import com.mojang.brigadier.arguments.StringArgumentType
 import com.mojang.brigadier.context.CommandContext
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback
 import net.minecraft.commands.CommandSourceStack
 import net.minecraft.commands.Commands
+import net.minecraft.commands.SharedSuggestionProvider
 import net.minecraft.network.chat.Component
 import net.minecraft.network.chat.TextColor
+import net.minecraft.server.level.ServerPlayer
+import net.minecraft.world.entity.EquipmentSlot
+import net.minecraft.world.item.ItemStack
 import pet.itpuppy.woofaddons.utils.Comp
 
 object ShowCommand : ServerCommand {
@@ -14,27 +19,53 @@ object ShowCommand : ServerCommand {
     override fun register() {
         CommandRegistrationCallback.EVENT.register { dispatcher, _, _ ->
             dispatcher.register(
-                Commands.literal("show").executes(::onExecuteCommand)
+                Commands.literal("show")
+                    .executes(::onExecuteCommand)
+                    .then(
+                        Commands.argument("type", StringArgumentType.word())
+                            .suggests { _, builder ->
+                                SharedSuggestionProvider.suggest(
+                                    ItemType.entries.map { type -> type.name.lowercase() },
+                                    builder
+                                )
+                            }
+                            .executes(::onExecuteCommand)
+                    )
             )
         }
     }
 
     override fun onExecuteCommand(ctx: CommandContext<CommandSourceStack>): Int {
+        val type = runCatching {
+            val input = StringArgumentType.getString(ctx, "type")
+            ItemType.entries.find { enum -> enum.name.equals(input, true) }
+        }.getOrNull() ?: ItemType.HELD
+
         val player = ctx.source.player ?: return 0
-        val heldItem = player.activeItem
+        val itemStack = type.getter(player)
         val broadcaster = ctx.source.server.playerList
 
         val message = Component.translatable(
-            "%s %s is holding %s",
+            "%s %s is %s %s",
 
             Comp.literal(shows.random(), true),
             Comp.buildUsernameComponent(player),
-            heldItem.displayName
+            type.holdingDescription,
+            itemStack.displayName
         ).withColor(TextColor.GRAY)
 
-        if (heldItem.count > 1) message.append(" (x${heldItem.count})")
+        if (itemStack.count > 1) message.append(" (x${itemStack.count})")
 
         broadcaster.broadcastSystemMessage(message, false)
         return 1
+    }
+
+    enum class ItemType(val holdingDescription: String, val getter: (ServerPlayer) -> ItemStack) {
+        HELD("holding", { it.mainHandItem }),
+        OFFHAND("holding", { it.offhandItem }),
+        HELMET("wearing", { it.getItemBySlot(EquipmentSlot.HEAD) }),
+        CHESTPLATE("wearing", { it.getItemBySlot(EquipmentSlot.CHEST) }),
+        LEGGINGS("wearing", { it.getItemBySlot(EquipmentSlot.LEGS) }),
+        BOOTS("wearing", { it.getItemBySlot(EquipmentSlot.FEET) })
     }
 }
